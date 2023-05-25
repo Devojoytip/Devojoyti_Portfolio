2f8efae4823cedd3eb2915f14fdf7efcee7c985b
Devojoytip <98347352+Devojoytip@users.noreply.github.com>
2023-05-26 00:00:47 +0530

    Fixed navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -27,10 +27,7 @@ const Navbar = () => {
 
   return (
     <nav
-      className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 
-      ${
-        scrolled ? "bg-primary" : "bg-transparent"
-      }`}
+      className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-white border-gray-200 dark:bg-gray-900`}
     >
       <div className='w-full flex justify-between items-center max-w-7xl mx-auto'>
         <Link

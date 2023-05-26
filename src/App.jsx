a6c89f42c1b1935e964cf64a282592055f978dbd
Devojoytip <98347352+Devojoytip@users.noreply.github.com>
2023-05-26 20:35:16 +0530

    Contact component built

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,6 @@ const App = () => {
         <About />
         <Tech />
         <Works />
-        <Feedbacks />
         <div className='relative z-0'>
           <Contact />
           <StarsCanvas />
